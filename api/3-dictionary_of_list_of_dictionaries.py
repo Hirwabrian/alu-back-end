ee3e6b1f181e60cb85eca0137636055acf7c1785
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-06-29 12:33:05 +0200

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,45 +6,32 @@ REST API and exports the tasks data to a JSON file.
 
 import json
 import requests
-import sys
 
-def get_employee_todo_progress(id):
-    todos = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
-
+def main():
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
     users_url = "https://jsonplaceholder.typicode.com/users"
+
     # Fetch all user data
     users_response = requests.get(users_url)
     users_data = users_response.json()
 
     # Fetch todo list 
-    todos_response = requests.get(todos)
+    todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
     # Prepare data for JSON export
-    tasks_by_user = []
+    all_tasks = {}
     for user in users_data:
-        user_id = user.get('id')
-        username = user.get('username')
-        tasks = [
-            {"username": username, "task": task.get('title'), "completed": task.get('completed')}
-            for task in todos_data if task.get('userId') == user_id
-        ]
-        tasks_by_user[str(user_id)] = tasks
-        
+        user_id = str(user['id'])
+        username = user['username']
+        user_tasks = [task for task in todos_data if task['userId'] == user['id']]
+        task_list = [{'username': username, 'task': task['title'], 'completed': task['completed']} for task in user_tasks]
+        all_tasks[user_id] = task_list
 
     # Write the result to a JSON file
     json_filename = "todo_all_employees.json"
     with open(json_filename, mode='w') as json_file:
-        json.dump(tasks_by_user, json_file)
+        json.dump(all_tasks, json_file)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(1)
-    
-    try:
-        id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
-    
-    get_employee_todo_progress(id)
+    main()
